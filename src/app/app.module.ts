74f0ef113e8157c2e22712aa40d8f06ee6a65956
Phuong BKA <phuongbka@Phuongs-MacBook-Pro.local>
2019-05-15 08:09:53 +1000

    skeleton page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,24 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ParentComponent } from "./parent/parent.component";
 import { ChildComponent } from "./child/child.component";
+import { topBannerNavComponent } from "./topBannerNav/topBannerNav.component";
+import { welcomePageComponent } from "./welcomePage/welcomePage.component";
+import { teamNavComponent } from "./teamNav/teamNav.component";
+
 import { TeamService } from "./team.service";
 import { HttpClientModule } from "@angular/common/http";
 
 import { RouterModule, Routes } from "@angular/router";
 
 @NgModule({
-  declarations: [AppComponent, ParentComponent, ChildComponent],
+  declarations: [
+    AppComponent,
+    ParentComponent,
+    ChildComponent,
+    topBannerNavComponent,
+    welcomePageComponent,
+    teamNavComponent
+  ],
   imports: [BrowserModule, FormsModule, AppRoutingModule, HttpClientModule],
   providers: [TeamService],
   bootstrap: [AppComponent]
---
+++ src/app/topBannerNav/topBannerNav.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'topBannerNav',
+  templateUrl: './topBannerNav.component.html',
+  styleUrls: ['./topBannerNav.component.css']
+})
+export class topBannerNavComponent  {
+  
+}
