fca0769d40f9bee77ed2f610ab776589fe20c846
Phuong BKA <phuongbka@Phuongs-MacBook-Pro.local>
2019-05-22 17:54:42 +1000

    add date-service, model, team results

--- src/app/team-results/team-results.component.ts
+++ src/app/team-results/team-results.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+import { DataService } from "../date-service.service";
+import { Team } from "../Model/team";
+import { Game } from "../Model/game";
+import { Tip } from "../Model/tip";
 
 @Component({
-  selector: 'app-team-results',
-  templateUrl: './team-results.component.html',
-  styleUrls: ['./team-results.component.css']
+  selector: "app-team-results",
+  templateUrl: "./team-results.component.html",
+  styleUrls: ["./team-results.component.css"]
 })
 export class TeamResultsComponent implements OnInit {
+  constructor(private dataService: DataService) {}
 
-  constructor() { }
+  teams: Team[];
+  games: Game[];
+  tips: Tip[];
 
   ngOnInit() {
+    this.getAFLTeams();
+    // this.getGames();
+    // this.getTips();
   }
 
+  getAFLTeams(): void {
+    this.dataService.getTeams().subscribe(temp => {
+      this.teams = temp;
+    });
+  }
+
+  // getGames(): void {
+  //   this.dataService.getGames().subscribe(temp => {
+  //     this.games = temp;
+  //   });
+  // }
+
+  // getTips(): void {
+  //   this.dataService.getTips().subscribe(temp => {
+  //     this.tips = temp;
+  //   });
+  // }
 }
