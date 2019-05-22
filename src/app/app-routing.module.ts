05dc0e895fdf7d160c4e294dcd5099dbb51df658
JamesHolborn <19670074@students.latrobe.edu.au>
2019-05-22 10:38:40 +1000

    gut

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,24 +2,25 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { Routes, RouterModule } from "@angular/router";
 
-import { ParentComponent } from "./parent/parent.component";
-import { welcomePageComponent } from "./welcomePage/welcomePage.component";
+
 import { UpcomingGamesComponent } from "./upcoming-games/upcoming-games.component";
 import { TeamResultsComponent } from "./team-results/team-results.component";
 import { ResultsByVenueComponent } from "./results-by-venue/results-by-venue.component";
 import { PredictionOfWinComponent } from "./prediction-of-win/prediction-of-win.component";
 import { NextFiveGamesComponent } from "./next-five-games/next-five-games.component";
-import { LoginScreenComponent } from "./login-screen/login-screen.component";
+import { RivalMatchesComponent } from './rival-matches/rival-matches.component';
+import { WinsLossesComponent } from './wins-losses/wins-losses.component';
+//import { LoginScreenComponent } from "./login-screen/login-screen.component";
 
 const routes: Routes = [
-  { path: "parent", component: ParentComponent },
   { path: "upcoming", component: UpcomingGamesComponent },
   { path: "team_results", component: TeamResultsComponent },
   { path: "results_by_venue", component: ResultsByVenueComponent },
   { path: "prediction", component: PredictionOfWinComponent },
   { path: "next_five_games", component: NextFiveGamesComponent },
-  { path: "", component: LoginScreenComponent },
-  { path: "welcome", component: welcomePageComponent }
+  { path: "rivals", component: RivalMatchesComponent },
+  { path: "wins_losses", component: WinsLossesComponent },
+  //{ path: "", component: LoginScreenComponent },
 ];
 
 @NgModule({
