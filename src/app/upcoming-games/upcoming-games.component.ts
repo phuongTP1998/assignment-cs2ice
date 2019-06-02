9a0e7ce141488af06f1117653ebec9be3876b9bc
Phuong BKA <phuongbka@Phuongs-MacBook-Pro.local>
2019-06-02 19:22:51 +1000

    upcoming games

--- src/app/upcoming-games/upcoming-games.component.ts
+++ src/app/upcoming-games/upcoming-games.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { DataService } from "../date-service.service";
+import { Game } from "../Model/game";
 
 @Component({
-  selector: 'app-upcoming-games',
-  templateUrl: './upcoming-games.component.html',
-  styleUrls: ['./upcoming-games.component.css']
+  selector: "app-upcoming-games",
+  templateUrl: "./upcoming-games.component.html",
+  styleUrls: ["./upcoming-games.component.css"]
 })
 export class UpcomingGamesComponent implements OnInit {
+  showSpinner: boolean;
+  games: Game[];
 
-  constructor() { }
+  constructor(private dataService: DataService) {}
 
   ngOnInit() {
+    this.getGames();
   }
 
+  getGames(): void {
+    this.showSpinner = true;
+    this.dataService
+      .getGames("https://api.squiggle.com.au/?q=games&year=2019&complete=0")
+      .subscribe(temp => {
+        this.games = temp;
+      })
+      .add(() => (this.showSpinner = false));
+  }
+  // not work
+  // sortById(games: Game[]) {
+  //   games.sort((gameA: Game, gameB: Game) => {
+  //     if (gameA.id > gameB.id) {
+  //       return 1;
+  //     }
+  //     if (gameA.id < gameB.id) {
+  //       return 0;
+  //     }
+  //     return 0;
+  //   });
+  // }
 }
