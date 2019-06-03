6b270777e925f75c97942b62ed2e4f93974c7774
Phuong BKA <phuongbka@Phuongs-MacBook-Pro.local>
2019-06-03 10:35:24 +1000

    adding last stories

--- src/app/prediction-of-win/prediction-of-win.component.ts
+++ src/app/prediction-of-win/prediction-of-win.component.ts
@@ -1,11 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 
 import { DataService } from "../date-service.service";
-import { Team } from "../Model/team";
-import { Game } from "../Model/game";
 import { Tip } from "../Model/tip";
-import { forEach } from '@angular/router/src/utils/collection';
-import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: "app-prediction-of-win",
@@ -17,45 +13,23 @@ export class PredictionOfWinComponent implements OnInit {
 
   showSpinner: boolean;
 
-  teams: Team[];
-  games: Game[];
   tips: Tip[];
 
   ngOnInit() {
-    this.getAFLTeams();
-    this.getGames();
     this.getTips();
-    this.findNextPrediction();
-  }
-
-  getAFLTeams(): void {
-    this.dataService.getTeams().subscribe(temp => {
-      this.teams = temp;
-    });
-  }
-
-  getGames(): void {
-    this.showSpinner = true;
-    this.dataService
-      .getGames("https://api.squiggle.com.au/?q=games;year=2019")
-      .subscribe(temp => {
-        this.games = temp.filter(
-          (team: any) =>
-            (team.complete == 100));
-      })
-      .add(() => (this.showSpinner = false));
   }
 
   getTips(): void {
     this.showSpinner = true;
-    this.dataService.getTips().subscribe(temp => {
-      this.tips = temp;
-    })
-    .add(() => (this.showSpinner = false));
+    this.dataService
+      .getTips()
+      .subscribe(temp => {
+        this.tips = temp;
+      })
+      .add(() => (this.showSpinner = false));
   }
 
-  findNextPrediction()
-  {
+  findNextPrediction() {
     /*
     if(this.tips.find( temp => temp.correct === 0 ) && (this.tips.find(temp=> temp.ateam == "hawthorn") || this.tips.find(temp=>temp.hteam == "hawthorn")))
     {
--- src/app/wins-losses/wins-losses.component.ts
+++ src/app/wins-losses/wins-losses.component.ts
@@ -1,66 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 import { DataService } from "../date-service.service";
-import { Team } from "../Model/team";
-import { Game } from "../Model/game";
-import { Tip } from "../Model/tip";
-import { forEach } from '@angular/router/src/utils/collection';
+import { Rank } from "../Model/rank";
+import { forEach } from "@angular/router/src/utils/collection";
 
 var stats = new Array(18);
 
 @Component({
-  selector: 'app-wins-losses',
-  templateUrl: './wins-losses.component.html',
-  styleUrls: ['./wins-losses.component.css']
+  selector: "app-wins-losses",
+  templateUrl: "./wins-losses.component.html",
+  styleUrls: ["./wins-losses.component.css"]
 })
 export class WinsLossesComponent implements OnInit {
   constructor(private dataService: DataService) {}
 
   showSpinner: boolean;
 
-  teams: Team[];
-  games: Game[];
-  tips: Tip[];
+  ladders: Rank[];
+  i: number;
 
   ngOnInit() {
-    this.getAFLTeams();
-    this.getGames();
-    this.getTips();
-   this.createStats();
+    this.i = 0;
+    this.getLadder();
   }
 
-  getAFLTeams(): void {
-    this.dataService.getTeams().subscribe(temp => {
-      this.teams = temp;
+  getLadder() {
+    this.dataService.getRank().subscribe(temp => {
+      this.ladders = temp;
     });
   }
-
-  getGames(): void {
-    this.showSpinner = true;
-    this.dataService
-      .getGames("https://api.squiggle.com.au/?q=games;year=2019")
-      .subscribe(temp => {
-        this.games = temp.filter(
-          (team: any) =>
-            (team.complete == 100));
-      })
-      .add(() => (this.showSpinner = false));
-  }
-
-  getTips(): void {
-    this.dataService.getTips().subscribe(temp => {
-      this.tips = temp;
-    });
-  }
-
-  
-  createStats(): void
-  {
-    /*
-    //var s = this.games.toString();
-    stats = this.games;
-    document.getElementById("printResults").innerHTML = stats["teamid"];
-     */
-  }
- 
 }
